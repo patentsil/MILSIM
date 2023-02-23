6d982cfad5c7eb957243309c9c3e405b6b17096c
glossypatience <45g2345g23@gmail.com>
2023-02-23 17:10:09 +0200

    Problem with Rigidbody3d
    it jumps to another location when it is unfrozen

---
+++ objects/test_colliding_object.gd
@@ -0,0 +1,12 @@
+extends RigidBody3D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	await get_tree().create_timer(4.0).timeout
+	freeze = false
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
